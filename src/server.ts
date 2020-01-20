a1b73cd472de5850bed10a5832fda1ba7a5b8fcd
Frederick Johnson <fritzj96@gmail.com>
2020-01-19 17:42:45 -0800

    added: server.ts to configure db & start app

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import 'dotenv/config';
+import 'reflect-metadata';
+import { createConnection } from "typeorm";
+
+import config from "./ormconfig";
+
+(async () => {
+  try {
+    await createConnection(config);
+  } catch (error) {
+    console.log("Error Connecting To The Database");
+    return error;
+  }
+  const app = new App(
+    [
+      new PostController(),
+    ]
+  );
+  app.listen();
+})();
