d499913d35e82d8706830aaeafbff32257f7a535
Frederick Johnson <fritzj96@gmail.com>
2020-01-20 22:14:32 -0800

    added: methods for postController

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -14,4 +14,80 @@ export class PostController implements Controller {
   constructor() {
     this.initializeRoutes();
   }
+
+  private initializeRoutes() {
+    this.router.post(
+      this.path,
+      validationMiddleware(CreatePostDto),
+      this.createPost
+    );
+    this.router.get(this.path, this.getAllPosts);
+    this.router.get(`${this.path}/:id`, this.getPostById);
+    this.router.patch(
+      `${this.path}/:id`,
+      validationMiddleware(CreatePostDto, true),
+      this.modifyPost
+    );
+    this.router.delete(`${this.path}/:id`, this.deletePost);
+  }
+
+  private createPost = async (
+    request: express.Request,
+    response: express.Response
+  ) => {
+    const postData: CreatePostDto = request.body;
+    const newPost = this.postRespository.create(postData);
+    await this.postRespository.save(newPost);
+    response.send(newPost);
+  };
+
+  private getAllPosts = async (
+    request: express.Request,
+    response: express.Response
+  ) => {
+    const posts = await this.postRespository.find();
+    response.send(posts);
+  };
+
+  private getPostById = async (
+    request: express.Request,
+    response: express.Response,
+    next: express.NextFunction
+  ) => {
+    const id = request.params.id;
+    const post = await this.postRespository.findOne(id);
+    if (post) {
+      response.send(post);
+    } else {
+      next(new PostNotFoundException(id));
+    }
+  };
+
+  private modifyPost = async (
+    request: express.Request,
+    response: express.Response,
+    next: express.NextFunction
+  ) => {
+    const id = request.params.id;
+    const postData: Post = request.body;
+    await this.postRespository.update(id, postData);
+    const updatedPost = await this.postRespository.findOne(id);
+    if (updatedPost) {
+      response.send(updatedPost);
+    } else {
+      next(new PostNotFoundException(id));
+    }
+  };
+
+  private deletePost = async (
+    request: express.Request,
+    response: express.Response,
+    next: express.NextFunction
+  ) => {
+    const id = request.params.id;
+    const deleteResponse = await this.postRespository.delete(id);
+    if (deleteResponse.raw[1]) {
+      response.sendStatus(200);
+    }
+  };
 }
